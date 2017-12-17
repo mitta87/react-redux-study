6e9cdab10e164bcc6047218006802b75fe747365
dpoletaev <poletaev87@gmail.com>
2017-12-17 23:34:56 +0200

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
 function counter(state = 0, action) {
     switch (action.type) {
@@ -11,10 +11,23 @@ function counter(state = 0, action) {
     }
 }
 
-let store = createStore(counter);
+function visibilityFilter(state = 'SHOW_ALL', action) {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter
+        default:
+            return state
+    }
+}
+
+let reducer = combineReducers({counter, visibilityFilter});
+let store = createStore(reducer);
 store.subscribe(() =>
     console.log(store.getState())
 );
 store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'DECREMENT' });
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+});
+
